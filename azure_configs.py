56583fcab4205082e237b078095c3da5c48fac46
steve <steve@contosofinancial>
2018-04-18 11:35:52 -0700

    adding some basic files to play around with the Azure file storage

---
+++ azure_configs.py
@@ -0,0 +1,7 @@
+import azure.storage.file.fileservice
+
+fs = azure.storage.file.fileservice.FileService(
+    account_name='sensitivedata', 
+    sas_token='?sv=2017-07-29&ss=f&srt=co&sp=rdl&se=2021-01-01T03:08:51Z&st=2018-04-18T18:08:51Z&spr=https&sig=vcFB1aGXYJPEDekInetHOb9TsMGBheJ6%2Bjb6R8SIemk%3D')
+
+fs.get_file_to_path('secrets', None, 'config.txt', 'config')
